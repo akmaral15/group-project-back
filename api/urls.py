83912d645b3592d7e77c4962b72081f00d07826e
akosya-oraz <60454740+akosya-oraz@users.noreply.github.com>
2020-04-26 22:13:23 +0300

    akw

--- api/urls.py
+++ api/urls.py
@@ -1,5 +1,9 @@
 from django.urls import path
 from api import views
 urlpatterns = [
-    path('categories/', views.CategoriesListView.as_view()),    
+    path('categories/', views.CategoriesListView.as_view()),
+    path('foods/', views.food_list_view),
+    path('foods/category/<int:id>/', views.FoodsByCategory.as_view()),
+    path('foods/<int:id>/', views.food_detailed_view),
+    
 ]
--- api/views.py
+++ api/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from rest_framework.views import APIView
+from rest_framework.decorators import api_view
 from django.http.response import JsonResponse
 from api.models import Category, Food
 from api.serializers import CategorySerializer, FoodSerializer
@@ -11,3 +12,32 @@ class CategoriesListView(APIView):
             return JsonResponse({"404": "no categories"}, safe=False)
         serializer = CategorySerializer(category_list, many=True)
         return JsonResponse(serializer.data, safe=False)
+
+@api_view(['GET'])
+def food_list_view(request):
+    try:
+        food_list = Food.objects.all()
+    except:
+        return JsonResponse({"404": "no food"}, safe=False)
+    serializer = FoodSerializer(food_list, many=True)
+    return JsonResponse(serializer.data, safe=False)
+        
+class FoodsByCategory(APIView):
+    def get(self, request, id):
+        try:
+            category = Category.objects.get(id=id)
+            food_list = category.food_set.all()
+        except:
+            return JsonResponse({"404": "no food"}, safe=False)
+        serializer = FoodSerializer(food_list, many=True)
+        return JsonResponse(serializer.data, safe=False)
+
+@api_view(['GET'])
+def food_detailed_view(request, id):
+    try:
+        food_detailed = Food.objects.get(id=id)
+    except:
+        return JsonResponse({"404": "no food"}, safe=False)
+    serializer = FoodSerializer(food_detailed)
+    return JsonResponse(serializer.data, safe=False)
+        
